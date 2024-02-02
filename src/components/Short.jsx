37f40699c996bd0585d1b4f96523eadfd9e98e8e
urooj khan <uroojk844@gmail.comm>
2024-02-02 19:05:54 +0530

    shorts running properly

--- src/components/Short.jsx
+++ src/components/Short.jsx
@@ -1,33 +1,59 @@
 import {
   FaEllipsisH,
   FaEye,
+  FaPlay,
   FaRegComment,
   FaRegHeart,
   FaShareAlt,
 } from "react-icons/fa";
 import CircularIcon from "./CirclularIcon";
-import { InView } from "react-intersection-observer";
+import { useEffect, useRef, useState } from "react";
 
 const Short = () => {
-  const toggleMode = (inView, entry) => {
-    console.log(entry);
-    if (inView) {
-      // entry.target.play();
-      entry.target.loop = true;
-    } else {
-      // entry.target.pause();
-      entry.target.loop = false;
-    }
+  const vid = useRef();
+  const [controls, setControls] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries[0].isIntersecting
+          ? entries[0].target.play()
+          : entries[0].target.pause();
+      },
+      {
+        threshold: 1,
+      }
+    );
+
+    observer.observe(vid.current);
+    setControls(vid.current.paused);
+
+    vid.current.on()
+  }, []);
+
+  const handleClick = () => {
+    controls ? vid.current.play() : vid.current.pause();
+    setControls((current) => !current);
   };
+
   return (
-    <div className="h-full flex justify-start relative">
+    <div className="video-container h-full flex justify-start relative">
       <div className="relative">
-        <InView
-          as="video"
-          onChange={(inView, entry) => toggleMode(inView, entry)}
-          src="/short.mp4"
-          className=" max-w-md w-full h-full object-cover snap-end snap-always"
-        ></InView>
+        <div className="relative h-full">
+          {controls && (
+            <div className="grid absolute inset-0 place-items-center">
+              <CircularIcon>
+                <FaPlay />
+              </CircularIcon>
+            </div>
+          )}
+          <video
+            ref={vid}
+            onClick={handleClick}
+            src="/video2.mp4"
+            className="video max-w-md w-full h-full object-cover snap-end snap-always"
+          ></video>
+        </div>
 
         {/* bottom content */}
         <div className="absolute bottom-0 w-full text-white p-4 gradient">
@@ -52,34 +78,34 @@ const Short = () => {
       <div className="absolute bottom-10 right-0 grid gap-4">
         <div className="text-white text-xs text-center grid gap-1">
           <CircularIcon>
-            <FaEye />
+            <FaEye size={18} />
           </CircularIcon>
           245K
         </div>
 
         <div className="text-white text-xs text-center grid gap-1">
           <CircularIcon>
-            <FaRegHeart />
+            <FaRegHeart size={18} />
           </CircularIcon>
           245K
         </div>
 
         <div className="text-white text-xs text-center grid gap-1">
           <CircularIcon>
-            <FaRegComment />
+            <FaRegComment size={18} />
           </CircularIcon>
           65k
         </div>
 
         <div className="text-white text-xs text-center grid gap-1">
           <CircularIcon>
-            <FaShareAlt />
+            <FaShareAlt size={18} />
           </CircularIcon>
           5k
         </div>
 
         <CircularIcon>
-          <FaEllipsisH />
+          <FaEllipsisH size={18} />
         </CircularIcon>
       </div>
     </div>
