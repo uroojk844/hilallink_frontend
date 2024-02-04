a1b75535a5b5e979bfe02816b2511a05ea7f7b88
Mohd Belal Naim <contactbelalnaim@gmail.com>
2024-02-04 14:17:14 +0530

    Added saved posts

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "../components/Navbar";
 import { usePathname } from "next/navigation";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,8 +12,12 @@ export default function RootLayout({ children }) {
   
   return (
     <html lang="en">
+      <Head>
+        <title>HilalLink</title>
+      </Head>
       <body className={`grid grid-row-max-auto ${inter.className}`}>
         {path != "/shorts" && <NavBar />}
+
         <main className="max-w-lg mx-auto w-full">{children}</main>
       </body>
     </html>
