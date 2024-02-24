e9e8c67b8c71c547b50eb795818c536937379458
Mohd Belal Naim <contactbelalnaim@gmail.com>
2024-02-24 15:52:15 +0530

    Fixed model issues

--- src/app/api/user/route.js
+++ src/app/api/user/route.js
@@ -1,12 +1,26 @@
 import { connectDB } from "@/mongo/database";
 import { userModel } from "@/mongo/models/user_model";
+import { NextResponse } from "next/server";
 
 connectDB();
 
 // add user
-export async function POST(res) {
-  const data = await res.json();
-  const user = new userModel(data);
-  const saved = await user.save();
-  return Response.json(saved);
+export async function POST(req) {
+  const data = await req.json();
+
+  return userModel
+    .findOne({ email: data.email })
+    .then((found) => {
+      if (found != null) return NextResponse.json({error:"User already exists"});
+      else {
+        const user = new userModel(data);
+        return user
+          .save()
+          .then((saved) => {
+            return NextResponse.json(saved);
+          })
+          .catch((err) => NextResponse.json("Already exist"));
+      }
+    })
+    .catch((err) => NextResponse.json(err));
 }
