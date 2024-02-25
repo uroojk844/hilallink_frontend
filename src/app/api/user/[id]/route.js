c9dd8d4fcf70f4e22cd1ae6a00a24cd3c0b5e9f8
Mohd Belal Naim <contactbelalnaim@gmail.com>
2024-02-25 12:24:22 +0530

    Added models for comments, notifications, and requests

--- src/app/api/user/[id]/route.js
+++ src/app/api/user/[id]/route.js
@@ -3,6 +3,7 @@ import { userModel } from "@/mongo/models/user_model";
 import { NextResponse } from "next/server";
 
 connectDB();
+
 export async function GET(req, res) {
   const user = await userModel.find({"username":res.params.id});
   return NextResponse.json(user);
