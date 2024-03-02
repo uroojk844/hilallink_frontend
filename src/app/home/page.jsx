906510e5a23bf419d632102e0031ec87a3b56f59
Mohd Belal Naim <belal@Mohds-MacBook-Air.local>
2024-03-02 10:58:18 +0530

    Fixed apos issue

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -16,7 +16,7 @@ const HomePage = () => {
         <section className="flex items-center gap-4 bg-white px-4 py-2">
           <img src="/avtar.jpg" className="h-8 w-8 rounded-full" alt="" />
           <div className="border-b w-full text-gray-500 border-gray-300 text-sm py-3">
-            Bismillah! What's on you mind?
+            Bismillah! What&apos;s on you mind?
           </div>
         </section>
         <section>
