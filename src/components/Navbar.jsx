835434add33bb04004cadaa2ef3d9eab0b4167b6
Mohd Belal Naim <belal@Mohds-MacBook-Air.local>
2024-03-03 13:35:01 +0530

    Added redux toggle for sidebar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,26 +8,22 @@ import {
   BsSearch,
 } from "react-icons/bs";
 import Link from "next/link";
-import {useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 
 import dynamic from "next/dynamic";
 import { FaBars, FaMosque } from "react-icons/fa";
 import { usePathname } from "next/navigation";
 import { twMerge } from "tailwind-merge";
+import { dispatch } from "@/redux/store";
+import { hideProfile, showProfile } from "@/redux/togglesSlice";
 const SideBar = dynamic(() => import("./SideBar"));
 const EditProfile = dynamic(() => import("./Navbar/EditProfile"));
-const SwicthProfile = dynamic(() => import("./Navbar/SwicthProfile"));
-const Login = dynamic(() => import("@/components/Auth/Login"));
-const Signup = dynamic(() => import("@/components/Auth/Signup"));
 
 const NavBar = () => {
   const editProfile = useSelector((state) => state.togglesSlice.editProfile);
-  const switchAccount = useSelector(
-    (state) => state.togglesSlice.switchProfile
-  );
-  const activeTab = usePathname()
+  const sidebar = useSelector((state) => state.togglesSlice.userProfile);
+  const activeTab = usePathname();
   return (
-
     <div className={"bg-white dark:bg-[hsl(0deg_0%_5%)] sticky top-0 z-40"}>
       <section className="container mx-auto  max-sm:border-b">
         <nav className="relative mx-auto flex items-center justify-between px-4 h-12">
@@ -92,22 +88,20 @@ const NavBar = () => {
             </Link>
           </div>
           <div className="flex items-center gap-4">
-
             <Link href="/notifications" className="sm:hidden">
               <BsBellFill size={22} className="text-gray-400" />
             </Link>
             <Link href="/chats" className="sm:hidden">
               <BsChatDots size={22} className="text-gray-400" />
             </Link>
-            <Link href="/chats" className="max-sm:hidden">
+            <div onClick={!sidebar?()=>dispatch(showProfile()):()=>dispatch(hideProfile())} className="max-sm:hidden cursor-pointer">
               <FaBars size={22} className="text-gray-400" />
-            </Link>
+            </div>
 
-            <SideBar />
+            {sidebar && <SideBar />}
           </div>
         </nav>
       </section>
-      {switchAccount && <SwicthProfile />}
       {editProfile && <EditProfile />}
     </div>
   );
