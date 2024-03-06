c4661bcff70c4bb3537b4b7b2431c9070bbc2e1e
urooj khan <uroojk844@gmail.comm>
2024-03-06 17:02:51 +0530

    adding middleware

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -17,7 +17,7 @@ const Login = () => {
     router.push("/");
   }
 
-  const{register,handleSubmit} = useForm()
+  const { register, handleSubmit } = useForm();
 
   const [btnWidth, setBtnWidth] = useState(200);
   const [email, setEmail] = useState("");
@@ -38,8 +38,8 @@ const Login = () => {
         "content-type": "application/json",
       },
       body: JSON.stringify({
-        email:data?.email,
-        password:data?.password,
+        email: data?.email,
+        password: data?.password,
       }),
     })
       .then((res) => res.json())
@@ -69,70 +69,67 @@ const Login = () => {
             "linear-gradient(rgba(0 0 0/0.6),rgba(0 0 0/0.6)),url('/profile.avif')",
         }}
       >
-          <form  onSubmit={handleSubmit(signin)}
-            id="form"
-            className="bg-white px-6 py-8 rounded-md w-[min(400px,96%)]"
-          
-          >
-            <div className="text-2xl font-bold">Welcome to HilalLink!</div>
-            <div className="mt-3">
-              <label htmlFor="" className="font-medium text-sm">
-                Email or Phone number
-              </label>
-              <input
-                {...register("email")}
-                type="email"
-                placeholder="Your email or phone number here"
-                className="w-full border p-2 rounded-md mb-4 max-sm:text-sm"
-                required
-              />
-              <label htmlFor="" className="font-medium text-sm ">
-                Password
-              </label>
-              <input
-                {...register("password")}
-                type="password"
-                placeholder="Your password here"
-                className="w-full border p-2 rounded-md max-sm:text-sm"
-                required
-              />
-              <div className="text-sm mt-1 font-medium flex justify-end">
-                Forgot Password?
-              </div>
-              {loading ? (
-                <button className="cursor-not-allowed flex justify-center w-full bg-black text-white rounded-md py-2.5 mt-4 text-sm">
-                  <TailSpin color="white" height={20} width={20} />
-                </button>
-              ) : (
-                <button
-                  className="w-full bg-black text-white rounded-md py-2.5 mt-4 text-sm"
-                >
-                  Sign in
-                </button>
-              )}
-
-              <div className="flex mt-2 justify-center">
-                <GoogleLogin
-                  theme="filled_black"
-                  width={btnWidth}
-                  text={"continue_with"}
-                  onSuccess={(res) => {
-                    googleLogin(res);
-                  }}
-                  onError={() => {
-                    console.log("Login Failed");
-                  }}
-                />
-              </div>
-
-              <Link href="/signup">
-                <div className="justify-center items-center mt-4 flex gap-2 text-sm">
-                  Create a new HilalLink Account <BsArrowRight />
-                </div>
-              </Link>
+        <form
+          onSubmit={handleSubmit(signin)}
+          id="form"
+          className="bg-white px-6 py-8 rounded-md w-[min(400px,96%)]"
+        >
+          <div className="text-2xl font-bold">Welcome to HilalLink!</div>
+          <div className="mt-3">
+            <label htmlFor="" className="font-medium text-sm">
+              Email or Phone number
+            </label>
+            <input
+              {...register("email")}
+              type="email"
+              placeholder="Your email or phone number here"
+              className="w-full border p-2 rounded-md mb-4 max-sm:text-sm"
+              required
+            />
+            <label htmlFor="" className="font-medium text-sm ">
+              Password
+            </label>
+            <input
+              {...register("password")}
+              type="password"
+              placeholder="Your password here"
+              className="w-full border p-2 rounded-md max-sm:text-sm"
+              required
+            />
+            <div className="text-sm mt-1 font-medium flex justify-end">
+              Forgot Password?
             </div>
-          </form>
-          
+            {loading ? (
+              <button className="cursor-not-allowed flex justify-center w-full bg-black text-white rounded-md py-2.5 mt-4 text-sm">
+                <TailSpin color="white" height={20} width={20} />
+              </button>
+            ) : (
+              <button className="w-full bg-black text-white rounded-md py-2.5 mt-4 text-sm">
+                Sign in
+              </button>
+            )}
+
+            <div className="flex mt-2 justify-center">
+              <GoogleLogin
+                theme="filled_black"
+                width={btnWidth}
+                text={"continue_with"}
+                onSuccess={(res) => {
+                  googleLogin(res);
+                }}
+                onError={() => {
+                  console.log("Login Failed");
+                }}
+              />
+            </div>
+
+            <Link href="/signup">
+              <div className="justify-center items-center mt-4 flex gap-2 text-sm">
+                Create a new HilalLink Account <BsArrowRight />
+              </div>
+            </Link>
+          </div>
+        </form>
       </div>
     </>
   );
