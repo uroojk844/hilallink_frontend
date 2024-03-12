cf894d2eb09105132a087cebf895e9dd39ee92e7
Mohd Belal Naim <contactbelalnaim@gamil.com>
2024-03-12 17:20:05 +0530

    Removed middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,27 +1,31 @@
-import { useParams } from "next/navigation";
-import { NextRequest, NextResponse } from "next/server";
-import * as jose from "jose";
-import bcrypt from "bcryptjs"
-
-
-const jwtConfig = {
-  secret: new TextEncoder().encode(process.env.JWT_SECRET),
-};
-
-export async function middleware(req) {
-  const token = req.cookies.get("token")?.value;
-  return jose
-    .jwtVerify(token, jwtConfig.secret)
-    .then((verified) => {
-      console.log("Token verified");
-      const uid = verified.payload.user
-      console.log(uid)
-      return NextResponse.next();
-    })
-    .catch((err) => {
-      return NextResponse.redirect(new URL("/auth/login", req.nextUrl));
-    });
+export async function middleware(req){
+  
 }
-export const config = {
-  matcher: ["/((?!api|_next|favicon.ico|profile.avif|auth/.).*)"],
-};
+
+// import { useParams } from "next/navigation";
+// import { NextRequest, NextResponse } from "next/server";
+// import * as jose from "jose";
+// import bcrypt from "bcryptjs"
+
+
+// const jwtConfig = {
+//   secret: new TextEncoder().encode(process.env.JWT_SECRET),
+// };
+
+// export async function middleware(req) {
+//   const token = req.cookies.get("token")?.value;
+//   return jose
+//     .jwtVerify(token, jwtConfig.secret)
+//     .then((verified) => {
+//       console.log("Token verified");
+//       const uid = verified.payload.user
+//       console.log(uid)
+//       return NextResponse.next();
+//     })
+//     .catch((err) => {
+//       return NextResponse.redirect(new URL("/auth/login", req.nextUrl));
+//     });
+// }
+// export const config = {
+//   matcher: ["/((?!api|_next|favicon.ico|profile.avif|auth/.).*)"],
+// };
