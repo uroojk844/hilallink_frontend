54d08b2c0d90923c220d8bbcc91f601c3fbcdf4e
Mohd Belal Naim <contactbelalnaim@gamil.com>
2024-03-13 13:46:44 +0530

    Added middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,31 +1,28 @@
-export async function middleware(req){
-  
+import { useParams } from "next/navigation";
+import { NextRequest, NextResponse } from "next/server";
+import * as jose from "jose";
+import bcrypt from "bcryptjs"
+
+
+const jwtConfig = {
+  secret: new TextEncoder().encode(process.env.JWT_SECRET),
+};
+
+export async function middleware(req) {
+  const token = req.cookies.get("token")?.value;
+  return jose
+    .jwtVerify(token, jwtConfig.secret)
+    .then((verified) => {
+      console.log("Token verified");
+      const uid = verified.payload.user
+      console.log(uid)
+      return NextResponse.next();
+    })
+    .catch((err) => {
+      return NextResponse.json(err)
+      // return NextResponse.redirect(new URL("/auth/login", req.nextUrl));
+    });
 }
-
-// import { useParams } from "next/navigation";
-// import { NextRequest, NextResponse } from "next/server";
-// import * as jose from "jose";
-// import bcrypt from "bcryptjs"
-
-
-// const jwtConfig = {
-//   secret: new TextEncoder().encode(process.env.JWT_SECRET),
-// };
-
-// export async function middleware(req) {
-//   const token = req.cookies.get("token")?.value;
-//   return jose
-//     .jwtVerify(token, jwtConfig.secret)
-//     .then((verified) => {
-//       console.log("Token verified");
-//       const uid = verified.payload.user
-//       console.log(uid)
-//       return NextResponse.next();
-//     })
-//     .catch((err) => {
-//       return NextResponse.redirect(new URL("/auth/login", req.nextUrl));
-//     });
-// }
-// export const config = {
-//   matcher: ["/((?!api|_next|favicon.ico|profile.avif|auth/.).*)"],
-// };
+export const config = {
+  matcher: ["/((?!api|_next|favicon.ico|profile.avif|auth/.).*)"],
+};
