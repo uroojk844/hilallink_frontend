7d098f3e2dbc1e960b2797425bda366da98035d6
urooj khan <uroojk844@gmail.comm>
2024-03-13 16:27:37 +0530

    FIX: checking username availibility before assigning new username

--- src/app/api/edit-profile/route.js
+++ src/app/api/edit-profile/route.js
@@ -3,15 +3,19 @@ import { NextResponse } from "next/server";
 
 export async function POST(req) {
   const data = await req.json();
-
-  return userModel
-    .findOneAndUpdate({ _id: data.user }, data, {
-      new: true,
-    })
-    .then((updatedData) => {
-      return NextResponse.json(updatedData);
-    })
-    .catch(() => {
-      return NextResponse.json({ error: "Something went wrong!!" });
-    });
+  const checkUsername = await userModel.exists({ username: data.username });
+  if (checkUsername != null) {
+    return NextResponse.json({ error: "This username already in use" });
+  } else {
+    return userModel
+      .findOneAndUpdate({ _id: data.user }, data, {
+        new: true,
+      })
+      .then((updatedData) => {
+        return NextResponse.json({ success: updatedData });
+      })
+      .catch(() => {
+        return NextResponse.json({ error: "Something went wrong!!" });
+      });
+  }
 }
